8345ada97e82014492f3e790f0462afd7c58ad76
Dallas Epperson <depperson@pcgus.com>
2019-04-10 23:16:13 -0400

    Added OSM map with arbitrary line overlay

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/hikes', function(req, res, next){
+  res.render('hikes', {
+    mapboxToken: process.env.MAPBOXTOKEN
+  });
 });
 
 module.exports = router;
