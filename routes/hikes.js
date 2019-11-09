52539a7829be7797148675be390a9b9b834382b7
Dallas Epperson <depperson@pcgus.com>
2019-11-08 21:27:03 -0500

    Added 2018.12.12 hike

--- routes/hikes.js
+++ routes/hikes.js
@@ -13,36 +13,36 @@ router.get('/', function(req, res, next){
 
 router.get('/api/items', function (req, res) {
   
-  db.all(`select id, startEpoch, name as "hikeName" from hike`, function(err, hikeRows){
-    hikeRows.forEach(function(a){
-      a.coords=[];
-      a.hikeDate = a.startEpoch;
-      a.features = [
-        {
-          "type":"Feature",
-          "geometry":{
-            "type":"LineString",
-            "coordinates":[]
-          }
-        }
-      ];
-    });
-    var hikeIds = hikeRows.map((a) => a.id).join(',');
-    db.all(`select * from coordinate where hikeId in (${hikeIds}) order by hikeId, ordinal`, function(err, coordRows){
-        coordRows.forEach(function(c){
-            let relevantHike = hikeRows.find((a) => a.id === c.hikeId);
-            relevantHike.features[0].geometry.coordinates.push([c.longitude, c.latitude]);
-        });
-        res.json(hikeRows);
-    });
-  });
+  // db.all(`select id, startEpoch, name as "hikeName" from hike`, function(err, hikeRows){
+  //   hikeRows.forEach(function(a){
+  //     a.coords=[];
+  //     a.hikeDate = a.startEpoch;
+  //     a.features = [
+  //       {
+  //         "type":"Feature",
+  //         "geometry":{
+  //           "type":"LineString",
+  //           "coordinates":[]
+  //         }
+  //       }
+  //     ];
+  //   });
+  //   var hikeIds = hikeRows.map((a) => a.id).join(',');
+  //   db.all(`select * from coordinate where hikeId in (${hikeIds}) order by hikeId, ordinal`, function(err, coordRows){
+  //       coordRows.forEach(function(c){
+  //           let relevantHike = hikeRows.find((a) => a.id === c.hikeId);
+  //           relevantHike.features[0].geometry.coordinates.push([c.longitude, c.latitude]);
+  //       });
+  //       res.json(hikeRows);
+  //   });
+  // });
 
 
     //TODO learn to read from DB instead
-    // fs.readFile('./data/hikes.json', function (err, json) {
-    //     let obj = JSON.parse(json);
-    //     res.json(obj);
-    // });
+    fs.readFile('./data/hikes.json', function (err, json) {
+        let obj = JSON.parse(json);
+        res.json(obj);
+    });
 });
   
 module.exports = router;
